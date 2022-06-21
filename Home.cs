af2fcefeea92b23db67af3c40d9fed09d5007330
eL-KoZar <85707056+eL-KoZar@users.noreply.github.com>
2022-06-21 15:41:42 +0200

    Add files via upload

---
+++ Home.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hospital_Management_System___HIT_Clinic
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 h = new Form1();
+            h.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Doctor d = new Doctor();
+            d.Show();
+            this.Hide();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Patient p = new Patient();
+            p.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Diagnosis d = new Diagnosis();
+            d.Show();
+            this.Hide();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
